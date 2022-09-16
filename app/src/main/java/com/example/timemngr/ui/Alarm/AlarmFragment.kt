cfd023a6d7658262830f3c5e17f5092206f1c37c
RennSter20 <88338561+RennSter20@users.noreply.github.com>
2022-09-16 22:41:30 +0200

    Edited alarm tab UI, added checkboxes for days of week, textview for selected time.

--- app/src/main/java/com/example/timemngr/ui/Alarm/AlarmFragment.kt
+++ app/src/main/java/com/example/timemngr/ui/Alarm/AlarmFragment.kt
@@ -1,19 +1,22 @@
 package com.example.timemngr.ui.Alarm
 
-import android.content.Intent
 import android.os.Bundle
-import android.provider.AlarmClock
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.timemngr.R
 import com.example.timemngr.databinding.FragmentHomeBinding
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
+import java.util.*
 
 class AlarmFragment : Fragment() {
 
@@ -40,26 +43,31 @@ class AlarmFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val picker =
-            MaterialTimePicker.Builder()
-                .setTimeFormat(TimeFormat.CLOCK_24H)
-                .setHour(12)
-                .setMinute(10)
-                .build()
+        val pickerTime = MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_24H).setHour(12).setMinute(10).build()
+        var selectedTimeText:TextView = requireView().findViewById(R.id.pickedTimeText)
 
-        var button:Button? = requireView().findViewById(R.id.containedButton)
 
-        button?.setOnClickListener(){
-            picker.show(parentFragmentManager, "alarmShow")
+        var selectTimeButton:Button? = requireView().findViewById(R.id.selectTimeButton)
+
+        //var checkboxDayOfWeek = mutableListOf(R.id.checkBoxMonday, R.id.checkBoxTuesday, R.id.checkBoxWednesday, R.id.checkBoxThursday, R.id.checkBoxFriday, R.id.checkBoxSaturday, R.id.checkBoxSunday)
+        var checkBoxMonday = R.id.checkBoxMonday
+        var checkBoxTuesday = R.id.checkBoxTuesday
+        var checkBoxWednesday = R.id.checkBoxWednesday
+        var checkBoxThursday = R.id.checkBoxThursday
+        var checkBoxFriday = R.id.checkBoxFriday
+        var checkBoxSaturday = R.id.checkBoxSaturday
+        var checkBoxSunday = R.id.checkBoxSunday
+
+        selectTimeButton?.setOnClickListener(){
+            pickerTime.show(parentFragmentManager, "alarmShow")
         }
 
-        picker?.addOnPositiveButtonClickListener {
-            Log.i("PICKER", "${picker?.hour}, ${picker?.minute}")
-            var alarmGo: Intent = Intent(AlarmClock.ACTION_SET_ALARM)
-            alarmGo.putExtra(AlarmClock.EXTRA_HOUR, picker?.hour)
-            alarmGo.putExtra(AlarmClock.EXTRA_MINUTES, picker?.minute)
-            startActivity(alarmGo)
+        pickerTime.addOnPositiveButtonClickListener {
+            selectedTimeText.text = pickerTime.hour.toString() + " : " + pickerTime.minute.toString()
         }
+
+
+
     }
 
     override fun onDestroyView() {
