94857c59d06508a413551f1289417f7cc2858729
RennSter20 <88338561+RennSter20@users.noreply.github.com>
2022-09-30 22:49:42 +0200

    Added timer layout.

--- app/src/main/java/com/package/timemngr/ui/timer/TimerFragment.kt
+++ app/src/main/java/com/package/timemngr/ui/timer/TimerFragment.kt
@@ -1,5 +1,6 @@
 package com.`package`.timemngr.ui.timer
 
+import android.graphics.Color
 import android.os.Bundle
 import android.transition.Explode
 import android.transition.Fade
@@ -8,12 +9,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.timemngr.R
 import com.example.timemngr.databinding.FragmentTimerBinding
-import org.w3c.dom.Text
 
 class TimerFragment : Fragment() {
 
@@ -47,6 +49,8 @@ class TimerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        var layoutOther:ConstraintLayout = requireView().findViewById(R.id.layout)
+
         var buttonZero = requireView().findViewById<Button>(R.id.button0)
         var buttonOne = requireView().findViewById<Button>(R.id.button)
         var buttonTwo = requireView().findViewById<Button>(R.id.button2)
@@ -59,90 +63,80 @@ class TimerFragment : Fragment() {
         var buttonNine = requireView().findViewById<Button>(R.id.button9)
         var buttonDelete = requireView().findViewById<Button>(R.id.buttonDelete)
 
-        var textHourOne:TextView = requireView().findViewById(R.id.textHourOne)
-        var textHourTwo:TextView = requireView().findViewById(R.id.textHourTwo)
+        var textHourDigits:TextView = requireView().findViewById(R.id.textHourDigits)
         var textHourLetter:TextView = requireView().findViewById(R.id.textHourLetter)
 
-        var textMinuteOne:TextView = requireView().findViewById(R.id.textMinuteOne)
-        var textMinuteTwo:TextView = requireView().findViewById(R.id.textMinuteTwo)
+        var textMinuteDigits:TextView = requireView().findViewById(R.id.textHourDigits)
         var textMinuteLetter:TextView = requireView().findViewById(R.id.textMinuteLetter)
 
-        var textSecondOne:TextView = requireView().findViewById(R.id.textSecondsOne)
-        var textSecondTwo:TextView = requireView().findViewById(R.id.textSecondsTwo)
+        var textSecondsDigits:TextView = requireView().findViewById(R.id.textSecondsDigits)
         var textSecondLetter:TextView = requireView().findViewById(R.id.textSecondsLetter)
 
 
-        val listOfText = arrayOf(textHourOne, textHourTwo, textMinuteOne, textMinuteTwo, textSecondOne, textSecondTwo)
 
-        var currentPosition = 5
+        var selectedDigits:TextView? = null
 
-        fun decrease(){
-            if(currentPosition > 0){
-                currentPosition--
-            }
+        fun setSelectedText(text:TextView){
+            textHourDigits.setTextColor(Color.CYAN)
+            selectedDigits = text
         }
 
-        fun increase(){
-            if(currentPosition < 5){
-                currentPosition++
-            }
+
+
+        textHourDigits.setOnClickListener(){
+            setSelectedText(textHourDigits)
+
+        }
+
+        textMinuteDigits.setOnClickListener(){
+            setSelectedText(textMinuteDigits)
+        }
+
+        textSecondsDigits.setOnClickListener(){
+            setSelectedText(textSecondsDigits)
         }
 
         buttonZero.setOnClickListener(){
-                if(currentPosition < 5){
-                    listOfText[currentPosition].setText("0")
-                    decrease()
-                }
+
         }
 
         buttonOne.setOnClickListener(){
-            listOfText[currentPosition].setText("1")
-            decrease()
+
         }
 
         buttonTwo.setOnClickListener(){
-            listOfText[currentPosition].setText("2")
-            decrease()
+
         }
 
         buttonThree.setOnClickListener(){
-            listOfText[currentPosition].setText("3")
-            decrease()
+
         }
 
         buttonFour.setOnClickListener(){
-            listOfText[currentPosition].setText("4")
-            decrease()
+
         }
 
         buttonFive.setOnClickListener(){
-            listOfText[currentPosition].setText("5")
-            decrease()
+
         }
 
         buttonSix.setOnClickListener(){
-            listOfText[currentPosition].setText("6")
-            decrease()
+
         }
 
         buttonSeven.setOnClickListener(){
-            listOfText[currentPosition].setText("7")
-            decrease()
+
         }
 
         buttonEight.setOnClickListener(){
-            listOfText[currentPosition].setText("8")
-            decrease()
+
         }
 
         buttonNine.setOnClickListener(){
-            listOfText[currentPosition].setText("9")
-            decrease()
+
         }
 
         buttonDelete.setOnClickListener(){
-
-
 
         }
     }
